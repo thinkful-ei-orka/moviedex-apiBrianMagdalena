42fcfaeae5a2f1855c707cae31bd85ba3681b7db
Magdalena Painter <paintermagdalena@gmail.com>
2020-06-02 16:06:37 -0400

    in progress

---
+++ moviedex-api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common')); // let's see what 'common' format looks like
+
+const movies = require('./movies-data');
+
+app.get('/movie', (req, res) => {
+  const { genre, contry, avg_vote} = req.query;
+  let newMovies = [...movies];
+
+if (genre) {
+    newMovies = newMovies.includes
+}
+
+  
+
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
